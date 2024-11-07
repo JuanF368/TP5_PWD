60bdba90a0c49baf39898dcb14a039cb32a6aaa3
JuanF368 <juanfarroni1314@gmail.com>
2024-11-07 15:54:17 -0300

    .

--- estructura/footer.php
+++ estructura/footer.php
@@ -2,6 +2,6 @@
     <div class="mt-5 p-5 text-center">
         <hr>
         <h6>Integrantes:</h6>
-        <p>Juan Farroni - Sabrina Gaspart - Rodrigo Martínez</p>
+        <p>**************</p>
     </div>
 </footer>
